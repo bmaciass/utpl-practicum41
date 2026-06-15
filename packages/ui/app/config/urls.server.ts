82e029b5c9a04c45237b141c67baf110ba61739b
bmaciass <bryanmacias1993@gmail.com>
2026-06-15 14:47:45 -0500

    adding staging env

--- packages/ui/app/config/urls.server.ts
+++ packages/ui/app/config/urls.server.ts
@@ -1,6 +1,9 @@
 export const IS_PRODUCTION = process.env.ENVIRONMENT === 'production'
 export const IS_DEVELOPMENT = process.env.ENVIRONMENT === 'development'
+export const IS_STAGING = process.env.ENVIRONMENT === 'staging'
 
 export const API_URL = IS_PRODUCTION
-  ? 'https://api.utpl-practicum.com/graphql'
-  : 'http://localhost:6002/graphql'
+  ? 'https://api.utpl-practicum.bmacias.dev/graphql'
+  : IS_STAGING
+    ? 'https://staging.api.utpl-practicum.bmacias.dev/graphql'
+    : 'http://localhost:6002/graphql'
